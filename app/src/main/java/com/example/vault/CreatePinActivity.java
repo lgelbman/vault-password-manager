54fc2293e7be6c2cb7054962d0c04cec7e8aedb6
yossi <yrotenstreich@gmail.com>
2021-01-04 17:30:56 -0500

    Wrote CreatePINActivity logic to create, validate, and save  PIN

--- app/src/main/java/com/example/vault/CreatePinActivity.java
+++ app/src/main/java/com/example/vault/CreatePinActivity.java
@@ -1,6 +1,8 @@
 package com.example.vault;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,9 +13,13 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreatePinActivity extends AppCompatActivity {
 
+    final String VALID_PIN_PATTERN = "\\d\\d\\d\\d";
     SharedPreferences sharedPreferences;
 
     @Override
@@ -23,18 +29,58 @@ public class CreatePinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_pin);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
-
+        setUpLayout();
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        fab.setOnClickListener(view -> Snackbar
+                .make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
+
+    private void setUpLayout() {
+        EditText enterPIN = findViewById(R.id.cp_enter_pin_et);
+        EditText confirmPIN = findViewById(R.id.cp_confirm_pin_et);
+
+        Button createButton = findViewById(R.id.cp_create_button);
+        createButton.setOnClickListener(v -> {
+            String pin1 = enterPIN.getText().toString();
+            String pin2 = confirmPIN.getText().toString();
+
+            if (isValidPIN(pin1, pin2)) {
+                savePIN(pin1);
+                startHomeActivity();
             }
         });
+    }
+
+    private boolean isValidPIN(String pin1, String pin2) {
+        TextView feedbackTV = findViewById(R.id.user_feedback_tv);
+
+        if (!pin1.equals(pin2)) {
+            // do something
+            feedbackTV.setTextColor(Color.RED);
+            feedbackTV.setText("PINs don't match");
+            return false;
+        }
+
+        if (!pin1.matches(VALID_PIN_PATTERN)) {
+            // do something
+            feedbackTV.setTextColor(Color.RED);
+            feedbackTV.setText("Invalid PIN pattern");
+            return false;
+        }
+        return true;
+    }
+
+    private void savePIN(String pin) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("VaultPIN", pin);
+        editor.apply();
+    }
+
+    private void startHomeActivity() {
+        Intent homeIntent = new Intent(this, HomeActivity.class);
+        startActivity(homeIntent);
+        finish();
     }
 }
